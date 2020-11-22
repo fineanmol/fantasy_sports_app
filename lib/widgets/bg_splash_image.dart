d47ea4a354f05676775d4987cac0dcd4631452d8
ANMOL AGARWAL <24794539+fineanmol@users.noreply.github.com>
2020-11-23 01:14:05 +0530

    init

--- lib/widgets/bg_splash_image.dart
+++ lib/widgets/bg_splash_image.dart
@@ -4,7 +4,7 @@ class BgSplashImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Image.asset(
-      "assets/bg.jpg",
+      "assets/bg_splash.jpg",
       fit: BoxFit.cover,
       color: Colors.black.withOpacity(0.7),
       colorBlendMode: BlendMode.darken,
